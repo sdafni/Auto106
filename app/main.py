4e9cf2297de06a068639c4bccea08c1ac85b6936
yuvaldafni <sdafni.yd@gmail.com>
2025-06-10 15:16:59 +0300

    #9 ui refactor

--- app/main.py
+++ app/main.py
@@ -160,6 +160,7 @@ def generate_stats_html() -> str:
 
 
 if __name__ == "__main__":
-    import uvicorn
+    pass
+    #import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
